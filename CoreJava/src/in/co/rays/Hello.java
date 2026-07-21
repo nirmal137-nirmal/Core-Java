9b76f79320500553afa5167f2328e2be45b0e50f
nirmal137-nirmal <nirmalfayke137@gmail.com>
2026-07-21 18:05:07 +0530

    ok

---
+++ CoreJava/src/in/co/rays/Hello.java
@@ -0,0 +1,9 @@
+package in.co.rays;
+
+public class Hello {
+	
+	public static void main(String[] args) {
+		System.out.println("Hello World");
+	}
+
+}
---
+++ CoreJava/src/in/co/rays/TestPrimeNumber.java
@@ -0,0 +1,25 @@
+package in.co.rays;
+
+public class TestPrimeNumber {
+
+	public static void main(String[] args) {
+
+		int number = 10;
+
+		int count = 0;
+
+		for (int i = 2; i < number; i++) {
+
+			if (number % i == 0) {
+				count++;
+				break;
+			}
+		}
+
+		if (count == 0) {
+			System.out.println("prime number");
+		} else {
+			System.out.println("not prime number");
+		}
+	}
+}

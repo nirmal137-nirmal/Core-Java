62e5b51621b512c8706adc4561eefe0e7527fee5
nirmal137-nirmal <nirmalfayke137@gmail.com>
2026-07-25 12:40:00 +0530

    commit

--- src/array/TestSecondMaxNumber.java
+++ src/array/TestSecondMaxNumber.java
@@ -12,13 +12,11 @@ public class TestSecondMaxNumber {
 				max = arr[i];
 			}	
 		}
-
 		for (int i = 0; i < arr.length; i++) {
 			if (arr[i] < max && arr[i] > secondmax) {
 				secondmax = arr[i];
 			}
 		}
 		System.out.println(secondmax);
-
 	}
 }

13d5590d55799754176c29eb38cce3667d2b7d4e
nirmal137-nirmal <nirmalfayke137@gmail.com>
2026-07-27 12:54:51 +0530

    Date and time code

---
+++ src/datetime/Testdemo.java
@@ -0,0 +1,13 @@
+package datetime;
+
+import java.util.Date;
+
+public class Testdemo {
+	public static void main(String[] args) {
+		
+		Date d = new Date();
+		
+		System.out.println("Current date is" + d);
+	}
+
+}

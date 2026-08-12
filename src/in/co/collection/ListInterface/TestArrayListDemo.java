854b45370add5f94a1b8d0194c8f0ff986b497c0
nirmal137-nirmal <nirmalfayake1307@gmail.com>
2026-08-12 17:31:52 +0530

    Collection

--- src/in/co/collection/ListInterface/TestArrayListDemo.java
+++ src/in/co/collection/ListInterface/TestArrayListDemo.java
@@ -9,19 +9,66 @@ public class TestArrayListDemo {
 		
 		List l = new ArrayList();
 		
-		l.add("Nirmal");
-		l.add("Rupesh");
-		l.add("Piyush");
-		l.add("Gourav");
-		l.add("Dipesh");
+		l.add(0, "Nirmal");
+		l.add(1, "Rupesh");
+		l.add(2, "Piyush");
+		l.add(3, "Gourav");
+		l.add(4, "Dipesh");
 		
 		//System.out.println(l);
 		
 		// Clear all Data
-		l.clear();
-		System.out.println(l);
+		/*
+		 * l.clear(); System.out.println(l);
+		 */
+		
+		//contains 
+		//System.out.println(l.contains(l)); // False
+		//System.out.println(l.contains("Nirmal"));
+		
+		// isEmpty method
+		//System.out.println(l.isEmpty()); // True or false
+		
+		//Remove element 
+		/*
+		 * l.remove("Nirmal"); System.out.println(l);
+		 */
+		
+		//System.out.println(l.size());
 		
 		
+		// Insert Bhsushan at position of index;
+		/*
+		 * l.add(5, "Bhushan"); System.out.println(l);
+		 */
+		
+		// Return the index element
+		
+		/*
+		 * System.out.println(l); System.out.println(l.get(2));
+		 */
+		
+		// remove 
+		
+		/*
+		 * l.remove(2); System.out.println(l);
+		 */
+		
+		// replace the i-th element with 0
+		
+		/*
+		 * l.set(3, "Vishal"); System.out.println(l);
+		 */
+		
+		
+		//Search object index
+		//System.out.println(l.indexOf("Gourav"));
+		
+		//searches object from end
+		//System.out.println(l.lastIndexOf("Nirmal"));
+		
+		l.subList(0, 4);
+		System.out.println(l.subList(0, 2));
 	}
 
 }

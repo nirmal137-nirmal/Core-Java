8b1e4466aa5354b4efbefe2d7d9fb2673cdceddf
nirmal137-nirmal <nirmalfayake1307@gmail.com>
2026-08-25 17:47:27 +0530

    commit

---
+++ src/in/co/corejava/array/TestMainMethod.java
@@ -0,0 +1,11 @@
+package in.co.corejava.array;
+
+public class TestMainMethod {
+	
+	public static void main(String[] args) {
+		
+		System.out.println("Name : " + args[0]);
+		System.out.println("Age : " + args[1]);
+	}
+
+}
